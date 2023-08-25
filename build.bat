e9392be105da5f4db26c58a552854c4f1997811d
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-24 21:47:42 -0400

    changed build systems they're now _slightly_ more generic ...I still don't know whether `build.bat` works

--- build.bat
+++ build.bat
@@ -4,5 +4,8 @@ set PDC="%PLAYDATE_SDK_PATH%\bin\pdc"
 set SIM="%PLAYDATE_SDK_PATH%\bin\PlaydateSimulator"
 set PDCFLAGS=-k
 
-%PDC% %PDCFLAGS% .\src\ .\Playtris.pdx
-if /i %1=="run" ( %SIM% .\Playtris.pdx )
+set IN=.\src
+set OUT=.\BlockDate.pdx
+
+%PDC% %PDCFLAGS% %IN% %OUT%
+if /i %1=="run" ( %SIM% %OUT% )
